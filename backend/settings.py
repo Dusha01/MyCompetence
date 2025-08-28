20e291f5a3bd754833c6cd9f7d6c1ae3133db5a8
Dusha01 <gubandr2806@gmail.com>
2025-08-28 15:40:08 +1000

    1

--- backend/settings.py
+++ backend/settings.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     telegram_bot_token: str = os.getenv("TELEGRAM_BOT_TOKEN", "")
     telegram_chat_id: str = os.getenv("TELEGRAM_CHAT_ID", "")
 
-    cors_origins: list = ["http://localhost:5173", "http://127.0.0.1:5173"]
+    cors_origins: list = ["http://localhost:5173", "http://127.0.0.1:5173", "https://dushafullstack.ru"]
 
     app_host: str = "127.0.0.1"
     app_port: int = 8000
