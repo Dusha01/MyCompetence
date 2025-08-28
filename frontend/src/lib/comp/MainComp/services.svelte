ec31a21b3b3137c226eb28ec3f81dea3e632ea45
Dusha01 <gubandr2806@gmail.com>
2025-08-28 15:27:52 +1000

    fx endpoint

--- frontend/src/lib/comp/MainComp/services.svelte
+++ frontend/src/lib/comp/MainComp/services.svelte
@@ -19,7 +19,7 @@
         submitError = false;
         
         try {
-            const response = await fetch('http://127.0.0.1:8000/api/contact', {
+            const response = await fetch('/api/contact', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
